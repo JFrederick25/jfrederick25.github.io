4164650fb494187a2d3789d59683ed40725d469d
Joey Frederick <jfrederick@armt.com>
2017-10-03 16:04:53 -0500

    modified

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+var chatMark = 0;
+
+function chat() {
+	setInterval(updateChat, 500);
+}
+
+function updateChat() {
+	// the first line of the text is the mark. it helps sync the messages.
+	var messageArray = getTxt("test.txt").split("\n");
+	var dataMark = parseInt(messageArray[0]);
+	messageArray.shift();
+	
+	var updateLines = [];
+	var updateChatLog = "";
+	if (dataMark != chatMark && (dataMark - chatMark) <= messageArray.length) {
+		for (i = chatMark - 1; i <= dataMark - 1; i++)
+		{
+			updateLines.push(messageArray[i]);
+		}
+		updateChatLog = '\n' + updateLines.toString().replace(/,/g, '');
+	}
+	chatMark = dataMark;
+	document.getElementById("box").innerHTML += updateChatLog;
+}
+
 function getTxt (path) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET", path, false);
@@ -7,9 +32,13 @@ function getTxt (path) {
 }
 
 function testTxt () {
-	alert("42");
+	return "42";
 }
 
-function submitTxt (data) {
+function submitTxt () {
+	var inputData = document.getElementById("input").value;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST", "test.txt", false);
+	xmlhttp.send(inputData);
 	
 }
