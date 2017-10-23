88989d428ba6e052c7130d56d2ea38f92989bf93
JFrederick25 <32333840+JFrederick25@users.noreply.github.com>
2017-10-23 16:29:38 -0500

    Getting rid of chat feature scripts

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-var chatMark = 0;
 
-function chat() {
-	setInterval(updateChat, 500);
-}
-
-function updateChat() {
-	// the first line of the text is the mark. it helps sync the messages.
-	var messageArray = getTxt("test.txt").split("\n");
-	var dataMark = parseInt(messageArray[0]);
-	messageArray.shift();
-	
-	var updateLines = [];
-	var updateChatLog = "";
-	if (dataMark != chatMark && (dataMark - chatMark) <= messageArray.length) {
-		for (i = chatMark - 1; i <= dataMark - 1; i++)
-		{
-			updateLines.push(messageArray[i]);
-		}
-		updateChatLog = '\n' + updateLines.toString().replace(/,/g, '');
-	}
-	chatMark = dataMark;
-	document.getElementById("box").innerHTML += updateChatLog;
-}
 
 function getTxt (path) {
 	var xmlhttp = new XMLHttpRequest();
@@ -33,12 +10,4 @@ function getTxt (path) {
 
 function testTxt () {
 	return "42";
-}
-
-function submitTxt () {
-	var inputData = document.getElementById("input").value;
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("POST", "test.txt", false);
-	xmlhttp.send(inputData);
-	
 }
